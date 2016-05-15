cd3f7c26a14e1e15ab616d4946b807b5b0713f32
Aidan Stern <aidans19@wildwood.org>
2016-05-15 14:22:54 -0700

    Initial Login Program in C#
    Made connection to SQL server

---
+++ Bensqloginwindowsforms/Bensqloginwindowsforms/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Bensqloginwindowsforms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection connection = new SqlConnection("Server = GMRDLT1; Database= BenloginDB; User = sa; Password = GreatMinds110;");
+            SqlCommand command = new SqlCommand("", connection);
+            SqlDataAdapter adapter;
+
+            DataTable table = new DataTable();
+            string username = textBox3.Text;
+            string password = textBox2.Text;
+
+            connection.Open();
+            command.CommandText = String.Format("INSERT Users VALUES('{0}','{1}')", username, password);
+            command.CommandType = System.Data.CommandType.Text;
+
+            command.ExecuteNonQuery();
+
+            //adapter = new SqlDataAdapter(command);
+            
+            connection.Close();
+
+
+              
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form2 Form = new Form2();
+            Form.ShowDialog();
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+
+
+        }
+    }
+}
---
+++ Bensqloginwindowsforms/Bensqloginwindowsforms/Form2.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Bensqloginwindowsforms
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection connection = new SqlConnection("Server = GMRDLT1; Database=BenloginDB; User = sa; Password = GreatMinds110;");
+            SqlCommand command = new SqlCommand("", connection);
+            SqlDataAdapter adapter;
+
+            DataTable table = new DataTable();
+            string username = textBox1.Text;
+            string password = textBox2.Text;
+
+            connection.Open();
+            command.CommandText = string.Format("SELECT * FROM Users WHERE Username = '{0}' AND Password = '{1}'", username, password);
+            command.CommandType = System.Data.CommandType.Text;
+
+            adapter = new SqlDataAdapter(command);
+            adapter.Fill(table);
+            connection.Close();
+
+           if(table.Rows.Count > 0)
+           {
+               label3.Visible = true;
+           }
+           else
+           {
+               label4.Visible = true;
+           }
+        
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+        
+
+    }
+}
